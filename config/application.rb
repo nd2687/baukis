7e59139ccc9146a709171532e330c34d24d1bb60
Takashi Kawano <kawano.fanta@gmail.com>
2014-09-20 13:52:36 +0900

    ジェネレーターの設定

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,12 @@ module Baukis
     config.i18n.load_path +=
       Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
+    config.generators do |g
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controller_specs false
+      g.view_specs false
+    end
   end
 end
