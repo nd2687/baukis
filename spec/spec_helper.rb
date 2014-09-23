c8c97a7738ad621f0edb3e9edcc904f5da4466d0
Takashi Kawano <kawano.fanta@gmail.com>
2014-09-23 12:41:58 +0900

    bug fixed テスト実験

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
-Rspec.configure do |config|
+RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.order = "random"
