d7d0cae5dfce87ba332664800a93a9952d921892
Takashi Kawano <kawano.fanta@gmail.com>
2014-09-23 13:09:31 +0900

    FactoryGirl 定義

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,4 +10,10 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.order = "random"
+
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    FactoryGirl.reload
+  end
 end
