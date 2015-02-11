20e3e4d9020e7a4857830f1e2fdbcd8580e021cc
Takashi Kawano <kawano.fanta@gmail.com>
2015-02-12 00:01:04 +0900

    ここから機能拡張編: 2.1顧客のログイン・ログアウト機能

--- app/controllers/customer/top_controller.rb
+++ app/controllers/customer/top_controller.rb
@@ -1,5 +1,8 @@
-class Customer::TopController < ApplicationController
+class Customer::TopController < Customer::Base
+  skip_before_action :authorize
+
   def index
-    raise ActiveRecord::RecordNotFound
+    render action: 'index'
+    # raise ActiveRecord::RecordNotFound
   end
 end
